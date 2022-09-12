b07bfb68e0f0e2e44eed504b2b5ae9247c11f36c
mizgin <mizgincvk@gmail.com>
2022-09-12 12:06:27 -0400

    IoC

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -1,10 +1,12 @@
 package com.cydeo.bean_annotation;
 
 import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.casefactory.DellCase;
 import com.cydeo.bean_annotation.config.ComputerConfig;
 import com.cydeo.bean_annotation.config.RandomConfig;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
+import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
 import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
@@ -19,14 +21,35 @@ public class ComputerTest {
         //Creating container by using BeanFactory
         BeanFactory context = new AnnotationConfigApplicationContext();
 
+        System.out.println("--------Retrieving the beans--------");
+
+        SonyMonitor sony = container.getBean(SonyMonitor.class);
+        DellCase dell = container.getBean(DellCase.class);
+        AsusMotherboard asus = container.getBean(AsusMotherboard.class);
+
+        PC pc = new PC(dell,sony,asus);
+
+        pc.powerUp();
+
+        dell.pressPowerButton();
+
+        System.out.println("--------Retrieving the beans--------");
+
 //        Monitor theMonitor = container.getBean(Monitor.class);
-        SonyMonitor theMonitor = container.getBean(SonyMonitor.class);
-        Case theCase = container.getBean(Case.class);
-        Motherboard theMotherboard = container.getBean(Motherboard.class);
+//        Case theCase = container.getBean(Case.class);
+//        Motherboard theMotherboard = container.getBean(Motherboard.class);
+//
+//        PC myPc = new PC(theCase,theMonitor,theMotherboard);
+//
+//        myPc.powerUp();
 
-        PC myPc = new PC(theCase,theMonitor,theMotherboard);
+        System.out.println("--------Multiple Objects--------");
 
-        myPc.powerUp();
+//        Monitor theMonitor2 = container.getBean("monitorSony", Monitor.class); //default bean name
+        Monitor theMonitor3 = container.getBean("sony", Monitor.class); //custom bean name
+        Monitor theMonitor4 = container.getBean(Monitor.class); //@Primary
+
+
 
 
 
