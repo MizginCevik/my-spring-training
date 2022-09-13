224f7f0b04f4e1d965383998637f4aecbcfc8609
mizgin <mizgincvk@gmail.com>
2022-09-13 15:38:54 -0400

    BeanScope

---
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,32 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    //private DBCommentRepository dbCommentRepository; --> it's tightly coupled
+    private final CommentRepository commentRepository; //always put here interface, because implementation can change
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    //                                                       @Qualifier("emailCommentNotificationProxy")
+    public CommentService(CommentRepository commentRepository, @Qualifier("email") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){ //it is business logic
+        //save in the DB
+        commentRepository.storeComment(comment);
+        //send email
+        commentNotificationProxy.sendComment(comment);
+
+
+
+
+    }
+}
