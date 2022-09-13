a1f6257950052d7103f83d087c752b5f683bdef6
mizgin <mizgincvk@gmail.com>
2022-09-13 14:46:01 -0400

    Abstraction

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApp.java
@@ -0,0 +1,10 @@
+package com.cydeo;
+
+public class CommentApp {
+
+    public static void main(String[] args) {
+
+
+        
+    }
+}
--- spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -4,14 +4,16 @@ import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import com.cydeo.repository.DBCommentRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommentService {
 
     //private DBCommentRepository dbCommentRepository; --> it's tightly coupled
-    private CommentRepository commentRepository;
+    private CommentRepository commentRepository; //always put here interface, because implementation can change
     private CommentNotificationProxy commentNotificationProxy;
 
-    public void publishComment(Comment comment){
+    public void publishComment(Comment comment){ //it is business logic
         //save in the DB
         commentRepository.storeComment(comment);
         //send email
