03bcd40801f6eab20fc9318240ee521de8c3e85d
mizgin <mizgincvk@gmail.com>
2022-09-20 15:23:34 -0400

    MVC Model Task

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -2,13 +2,15 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CarController {
 
-    @RequestMapping("/info") //write this localhost:8080/info?make=Tesla on browser
+    //*****(QUERY PARAMETER)
+    @RequestMapping("/info") //write this localhost:8080/info?make=Tesla on browser (QUERY PARAMETER)
     public String carInfo(@RequestParam String make, Model model){
 
         model.addAttribute("make", make);
@@ -17,7 +19,8 @@ public class CarController {
         return "car/car-info";
     }
 
-    @RequestMapping("/info2") //write this localhost:8080/info2 -> (KIA)
+    //*****(QUERY PARAMETER)
+    @RequestMapping("/info2") //write this localhost:8080/info2 -> (KIA) (QUERY PARAMETER)
     public String carInfo2(@RequestParam(value = "make", required = false, defaultValue = "KIA") String make, Model model){
 
         model.addAttribute("make", make);
@@ -26,6 +29,7 @@ public class CarController {
         return "car/car-info";
     }
 
+    //*****(QUERY PARAMETER)
     @RequestMapping("/info3") //write this localhost:8080/info3?make=Tesla&year=2020 on browser
     public String carInfo3(@RequestParam String make, @RequestParam int year, Model model){
 
@@ -33,6 +37,15 @@ public class CarController {
         model.addAttribute("year", year);
 
 
+        return "car/car-info";
+    }
+
+    //*****(PATH VARIABLE)
+    @RequestMapping("/info/{make}/{year}") //localhost:8080/info/honda/2015
+    public String getCarInfo(@PathVariable String make, @PathVariable int year){
+
+        System.out.println(make);
+        System.out.println(year);
         return "car/car-info";
     }
 }
