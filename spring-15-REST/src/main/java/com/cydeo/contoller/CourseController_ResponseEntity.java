f7dce1b9317da5bcb9e4641d1137cd3796a26217
mizgin <mizgincvk@gmail.com>
2022-11-27 15:58:02 -0500

    created APIs with three ways

---
+++ spring-15-REST/src/main/java/com/cydeo/contoller/CourseController_ResponseEntity.java
@@ -0,0 +1,49 @@
+package com.cydeo.contoller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses/api/v2")
+public class CourseController_ResponseEntity {
+
+    private final CourseService courseService;
+
+    public CourseController_ResponseEntity(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    // ResponseEntity is used to pass header, to modify API responses we are sending
+    @GetMapping
+    public ResponseEntity<List<CourseDTO>> getAllCourses() {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Version","Cydeo.V2")
+                .header("Operation","Get List")
+                .body(courseService.getCourses());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") long courseId) {
+        return ResponseEntity.ok(courseService.getCourseById(courseId));
+    }
+
+    @GetMapping("category/{name}")
+    public ResponseEntity<List<CourseDTO>> getCourseByCategory(@PathVariable("name") String category) {
+        return ResponseEntity.ok(courseService.getCoursesByCategory(category));
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO course) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Operation","Create")
+                .body(courseService.createCourse(course));
+    }
+
+}
