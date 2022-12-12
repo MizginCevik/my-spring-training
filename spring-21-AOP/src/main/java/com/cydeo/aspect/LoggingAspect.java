80d872874e69c415e397442004fcd1e58ac6d04a
mizgin <mizgincvk@gmail.com>
2022-12-11 23:42:54 -0500

    spring-21-AOP LoggingAspect first examples

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -8,26 +8,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Aspect
+@Aspect // It is used for Aspect class, no bean is created for Aspect
 @Component
 public class LoggingAspect {
 
-    Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+    Logger logger = LoggerFactory.getLogger(LoggingAspect.class); // logger field is used to put information on console
 
-//    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
-//    public void myPointcut() {}
-//
-//    @Before("myPointcut()")
+    //  Creating Pointcut means defining my JoinPoints
+    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
+    public void myPointcut() {}
+
+    // This is Advice. Defining here what we need to do
+    @Before("myPointcut()") // this method will run before any method runs in Course Controller
+    public void log() {
+        logger.info("Info log.......");
+    }
+
+//    @Before("execution(* com.cydeo.controller.CourseController.*(..))") // second way without defining Pointcut
 //    public void log() {
 //        logger.info("Info log.......");
 //    }
-//
-//    @Before("execution(* com.cydeo.controller.CourseController.*(..))")
-//    public void log() {
-//        logger.info("Info log.......");
-//    }
 
-    @Pointcut("execution(* com.cydeo.repository.CourseRepository.*(..))")
+    // put log information before findById method inside CourseRepository class
+    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))") // single parameter
     public void courseRepositoryFindByIdPC() {}
 
     @Before("courseRepositoryFindByIdPC()")
@@ -35,5 +38,8 @@ public class LoggingAspect {
         logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
                 , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
     }
-
+    // JoinPoints are the methods that we want to cross counting concerns before or after
+    // if {} is used in logger methods, arguments will be replaced
+    // the log on the console :
+    // Before -> Method: Optional org.springframework.data.repository.CrudRepository.findById(Object), Arguments: [2], Target: org.springframework.data.jpa.repository.support.SimpleJpaRepository@29c127ee
 }
